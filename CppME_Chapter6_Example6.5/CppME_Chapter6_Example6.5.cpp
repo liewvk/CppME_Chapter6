f2792716d302777edaa320e8fea32e1ddfcc567d
Dr.Liew vk <42384837+liewvk@users.noreply.github.com>
2026-09-20 11:45:30 +0800

    Added Files

---
+++ CppME_Chapter6_Example6.5/CppME_Chapter6_Example6.5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1 = 15;
+    int num2 = 4;
+
+    cout << "Number 1   = " << num1 << endl;
+    cout << "Number 2   = " << num2 << endl;
+    cout << "Addition   = " << num1 + num2 << endl;
+    cout << "Subtraction= " << num1 - num2 << endl;
+    cout << "Multiply   = " << num1 * num2 << endl;
+    cout << "Division   = " << num1 / num2 << endl;
+    cout << "Remainder  = " << num1 % num2 << endl;
+
+    return 0;
+}
---
+++ CppME_Chapter6_Example6.8/CppME_Chapter6_Example6.8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int marks = 75;
+
+    cout << "marks == 75 : " << (marks == 75) << endl;
+    cout << "marks != 50 : " << (marks != 50) << endl;
+    cout << "marks > 60  : " << (marks > 60) << endl;
+    cout << "marks < 40  : " << (marks < 40) << endl;
+    cout << "marks >= 75 : " << (marks >= 75) << endl;
+    cout << "marks <= 80 : " << (marks <= 80) << endl;
+
+    return 0;
+}
